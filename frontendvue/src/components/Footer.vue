4c2c92a43e23f36d4be88b47e5825f5fe7be4d08
Alvaro Gabriel Jurado Huarachi <alvaro.jurado@ucb.edu.bo>
2024-05-31 13:54:10 -0400

    Mejora de diseño

--- frontendvue/src/components/Footer.vue
+++ frontendvue/src/components/Footer.vue
@@ -1,40 +1,40 @@
 <template>
-    <div class="bg-blue-400 md:px-14 p-4 max-w-screen-2xl mx-auto text-white">
-     
-      <div class="flex flex-col sm:flex-row gap-8 sm:items-center justify-between my-8">
-        <p class="text-center py-4">© {{ currentYear }} Los Puni - Sistemas de información III</p>
-        <p class="text-primary font-semibold text-base dark:text-white">Hecho con {"<3"}</p>
-        <div class="flex items-center space-x-5">
-          <img :src="fb" alt="Facebook" class="w-8 cursor-pointer hover:-translate-y-4 transition-all duration-300" />
-          <img :src="ig" alt="Instagram" class="w-8 cursor-pointer hover:-translate-y-4 transition-all duration-300" />
-          <img :src="tw" alt="Twitter" class="w-8 cursor-pointer hover:-translate-y-4 transition-all duration-300" />
-          <img :src="tt" alt="TikTok" class="w-8 cursor-pointer hover:-translate-y-4 transition-all duration-300" />
-        </div>
+  <div class="bg-gradient-to-r from-blue-500 to-purple-600 md:px-14 p-4 max-w-screen-2xl mx-auto text-white">
+    <div class="flex flex-col sm:flex-row gap-8 sm:items-center justify-between my-8">
+      <p class="text-center py-4">© {{ currentYear }} Los Puni - Sistemas de información III</p>
+      <p class="text-primary font-semibold text-base dark:text-white">Hecho con {"<3"}</p>
+      <div class="flex items-center space-x-5">
+        <img :src="fb" alt="Facebook" class="w-8 cursor-pointer hover:-translate-y-4 transition-all duration-300" />
+        <img :src="ig" alt="Instagram" class="w-8 cursor-pointer hover:-translate-y-4 transition-all duration-300" />
+        <img :src="tw" alt="Twitter" class="w-8 cursor-pointer hover:-translate-y-4 transition-all duration-300" />
+        <img :src="tt" alt="TikTok" class="w-8 cursor-pointer hover:-translate-y-4 transition-all duration-300" />
       </div>
     </div>
-  </template>
-  
-  <script>
-  import fb from '../assets/fb.png';
-  import ig from '../assets/ig.png';
-  import tw from '../assets/tw.png';
-  import tt from '../assets/tt.png';
-  
-  export default {
-    name: 'Footer',
-    data() {
-      return {
-        fb,
-        ig,
-        tw,
-        tt,
-        currentYear: new Date().getFullYear()
-      };
-    }
-  };
-  </script>
-  
-  <style scoped>
-  /* Estilos adicionales si es necesario */
-  </style>
-  
+  </div>
+</template>
+
+<script>
+import fb from '../assets/fb.png';
+import ig from '../assets/ig.png';
+import tw from '../assets/tw.png';
+import tt from '../assets/tt.png';
+
+export default {
+  name: 'Footer',
+  data() {
+    return {
+      fb,
+      ig,
+      tw,
+      tt,
+      currentYear: new Date().getFullYear()
+    };
+  }
+};
+</script>
+
+<style scoped>
+.bg-gradient-to-r {
+  background: linear-gradient(to right, var(--tw-gradient-stops));
+}
+</style>
--- frontendvue/src/components/Menu.vue
+++ frontendvue/src/components/Menu.vue
@@ -1,47 +1,47 @@
 <template>
-    <div class="container max-w-6xl mx-auto  flex flex-col items-center ">
-      <div class="container max-w-6xl bg-gray-800  border border-gray-700 rounded-lg p-6 shadow-lg text-center w-full max-w-lg">
-        <h1 class="text-2xl font-bold text-white mb-4">Menu WEKA</h1>
-        <img src="@/assets/weka-logo.png" alt="WEKA Logo" class="mx-auto mb-6 w-24">
-        <div class="button-list flex flex-col gap-6 w-full">
-          <div class="button-item flex flex-col items-center gap-4 text-white">
-            <p>
-              La edición del archivo CSV permite modificar y actualizar los datos cargados previamente para mejorar la precisión del análisis.
-            </p>
-            <div class="flex justify-center">
-              <button @click="navigate('edit-csv')" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Edit CSV</button>
+    <div class="min-h-screen bg-gradient-to-r from-gray-800 via-gray-600 to-gray-400 p-4">
+      <div class="container max-w-6xl mx-auto flex flex-col items-center">
+        <div class="container max-w-6xl bg-gray-800 border border-gray-700 rounded-lg p-6 shadow-lg text-center w-full max-w-lg">
+          <h1 class="text-2xl font-bold text-white mb-4">Menu WEKA</h1>
+          <img src="@/assets/weka-logo.png" alt="WEKA Logo" class="mx-auto mb-6 w-24">
+          <div class="button-list flex flex-col gap-6 w-full">
+            <div class="button-item flex flex-col items-center gap-4 text-white">
+              <p>
+                La edición del archivo CSV permite modificar y actualizar los datos cargados previamente para mejorar la precisión del análisis.
+              </p>
+              <div class="flex justify-center">
+                <button @click="navigate('edit-csv')" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Edit CSV</button>
+              </div>
             </div>
-          </div>
-          <div class="button-item flex flex-col items-center gap-4 text-white">
-            <p>
-              La clasificación utilizando árboles de decisión, como J48, construye un modelo predictivo que mapea observaciones sobre un ítem a conclusiones sobre el valor del ítem objetivo.
-            </p>
-            <div class="flex justify-center">
-              <button @click="navigate('classify-decision-trees')" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Classify: Decision Trees</button>
+            <div class="button-item flex flex-col items-center gap-4 text-white">
+              <p>
+                La clasificación utilizando árboles de decisión, como J48, construye un modelo predictivo que mapea observaciones sobre un ítem a conclusiones sobre el valor del ítem objetivo.
+              </p>
+              <div class="flex justify-center">
+                <button @click="navigate('classify-decision-trees')" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Classify: Decision Trees</button>
+              </div>
             </div>
-          </div>
-          <div class="button-item flex flex-col items-center gap-4 text-white">
-            <p>
-              La clasificación utilizando funciones, como el Perceptrón Multicapa, utiliza una red neuronal para clasificar los datos en función de un conjunto de entradas y salidas esperadas.
-            </p>
-            <div class="flex justify-center">
-              <button @click="navigate('classify-functions')" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Classify: Functions</button>
+            <div class="button-item flex flex-col items-center gap-4 text-white">
+              <p>
+                La clasificación utilizando funciones, como el Perceptrón Multicapa, utiliza una red neuronal para clasificar los datos en función de un conjunto de entradas y salidas esperadas.
+              </p>
+              <div class="flex justify-center">
+                <button @click="navigate('classify-functions')" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Classify: Functions</button>
+              </div>
             </div>
-          </div>
-          <div class="button-item flex flex-col items-center gap-4 text-white">
-            <p>
-              El clustering utilizando KMeans agrupa los datos en K clusters distintos, donde cada dato pertenece al cluster con la media más cercana, ayudando a descubrir estructuras ocultas en los datos.
-            </p>
-            <div class="flex justify-center">
-              <button @click="navigate('cluster')" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Cluster</button>
-
+            <div class="button-item flex flex-col items-center gap-4 text-white">
+              <p>
+                El clustering utilizando KMeans agrupa los datos en K clusters distintos, donde cada dato pertenece al cluster con la media más cercana, ayudando a descubrir estructuras ocultas en los datos.
+              </p>
+              <div class="flex justify-center">
+                <button @click="navigate('cluster')" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Cluster</button>
+              </div>
             </div>
           </div>
         </div>
       </div>
     </div>
   </template>
-  
   
   <script>
   export default {
@@ -54,8 +54,6 @@
   </script>
   
   <style scoped>
-    /* background-color: #f3f4f6; */
-    
   /* Puedes agregar estilos adicionales si es necesario */
   </style>
   
